cd0dba68094fbf007829deddf88b07899b1273c2
AkaroNamira <steven.p.important@gmail.com>
2021-10-06 10:07:29 +0200

    vaja2

---
+++ Vaja2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+       /* 3. Podano je zaporedje: 2, 2, 3, 5, 14, 69, 965, ...
+          a.Z uporabo rekurzije napišite metodo, ki vrne n-ti element zaporedja.
+          b.Napišite sled izvajanja programa za klic test.clen(2) in test.clen(5)!
+          Upoštevajte, da je zaporedje definirano:
+            a1 = 2
+            a2 = 2
+            a(n) = a(n-2)*a(n-1) – 1*/
+       static int 
+
+        /*4. Podano je zaporedje: 2, 8, 26, 80, 242, ...
+        a.Z uporabo rekurzije napišite metodo, ki vrne n-ti element zaporedja.
+        b.Napišite sled izvajanja programa za klic test.clen(2) in test.clen(5).
+        Upoštevajte, da je zaporedje definirano:
+        a1 = 2
+        a(n) = 3 * a(n-1) + 2*/
+
+
+        /*5. Podano je zaporedje:: 1, 2, 5, 12, 29, …
+        a.Z uporabo rekurzije napišite metodo, ki vrne n-ti element zaporedja.
+        Upoštevajte, da je zaporedje definirano:
+        a1 = 1
+        a2 = 2
+        a(n) = a(n-1)*2 + a(n-2)*/
+    }
+}
