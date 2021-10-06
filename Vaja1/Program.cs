3915517cf2e33f83190e3ff63738544cefaf989c
AkaroNamira <steven.p.important@gmail.com>
2021-10-06 09:59:55 +0200

    Vaja1

--- Vaja1/Program.cs
+++ Vaja1/Program.cs
@@ -10,8 +10,31 @@ namespace Vaja1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine(PotencaIterativno(10));
+            Console.WriteLine(PotencaRekurzivno(10));
+            Console.WriteLine(Fib(7));
             Console.ReadLine();
+        }
+        static int Fib(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+        static int PotencaIterativno(int n)
+        {
+            int potenca = 1;
+            for(int k = 1; k <= n; k++)
+            {
+                potenca = potenca * 2;
+            }
+            return potenca;
+        }
+        static int PotencaRekurzivno(int n)
+        {
+            if (n == 0)
+                return 1;
+            return 2 * PotencaRekurzivno(n - 1);
         }
     }
 }
